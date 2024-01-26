b07898790bba20867d27bd76cfab2ae29808be17
jstar_00 <102504764+jstar000@users.noreply.github.com>
2024-01-26 14:28:40 +0900

    Project 4 - Challenge 전까지

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,40 +9,44 @@ import CoreML //보통 여러 개 import할 때는 알파벳 순서로 import
 import SwiftUI
 
 struct ContentView: View {
-    static var defaultWakeUpTime: Date {
-        var component = DateComponents()
-        component.hour = 7
-        component.minute = 0
-        return Calendar.current.date(from: component) ?? Date.now
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
     }
     
     @State private var sleepAmount = 8.0 //얼마나 자고싶은지
-    @State private var wakeUp = defaultWakeUpTime //언제 일어나고싶은지
+    @State private var wakeUp = defaultWakeTime //언제 일어나고싶은지
     @State private var coffeeAmount = 1 //하루에 커피를 얼마나 마시고싶은지
     
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
-    
+
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("When do you want to wake up?")
-                    .font(.headline)
+            Form {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                    //언제 일어나고 싶은지 날짜가 아니라 '시간'이 궁금하므로 .hourAndMinute 선택
+                }
                 
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                //언제 일어나고 싶은지 날짜가 아니라 '시간'이 궁금하므로 .hourAndMinute 선택
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
                 
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-                
-                Text("Daily coffee intake")
-                    .font(.headline)
-                
-                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20)
+                    //WTF ->
+                }
             }
             .navigationTitle("BetterRest")
             .toolbar {
@@ -64,7 +68,7 @@ struct ContentView: View {
             //1. SleepCalculator클래스의 인스턴스 생성하기(configuration은 '구성'이라는 뜻)
             //-> 이 모델 인스턴스로 데이터를 읽은 후 예측 데이터를 만들 수 있음
             //do-catch문은 Core ML이 두 군데서 에러를 만들 수 있으므로 사용
-            //-> model을 load할 때와(바로 위 코드), 예측 데이터를 만들 때
+            //<- model을 load할 때와(바로 위 코드), 예측 데이터를 만들 때 에러 가능
             
             let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
             //여기서 [.hour, .minute]이라는 건, dateComponent에는 year, month, day, hour, minute, second, timezone 등 다양한 component가 있는데 거기서 hour와 minute component를 얻어오겠다는 의미로 사용되는 듯!
