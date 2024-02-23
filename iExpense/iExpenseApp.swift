70de50f2e2de36b613e5c46728e1d8901aa5bad8
jstar_00 <102504764+jstar000@users.noreply.github.com>
2024-02-23 17:45:50 +0900

    Project 7 - Building a list we can delete from까지

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by 임지성 on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
