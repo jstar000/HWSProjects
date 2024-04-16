3d93325a0ddb79ff35fe8ca1341200e986e9e267
jstar_00 <102504764+jstar000@users.noreply.github.com>
2024-04-16 22:44:43 +0900

    Initial Commit

---
+++ CupcakeCorner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by 임지성 on 4/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by 임지성 on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
