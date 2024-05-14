7137de3226914724eebebdcfe4b79f6fdfca5958
jstar_00 <102504764+jstar000@users.noreply.github.com>
2024-05-15 06:26:31 +0900

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by 임지성 on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
