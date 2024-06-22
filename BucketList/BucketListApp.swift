26911a28ba14e84573ec259a1157a42d3b205533
jstar_00 <102504764+jstar000@users.noreply.github.com>
2024-06-22 09:33:51 +0900

    Project 14 - Improving our map annotations 까지 완료

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by 임지성 on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketList/Location.swift
@@ -0,0 +1,32 @@
+//
+//  Location.swift
+//  BucketList
+//
+//  Created by 임지성 on 6/19/24.
+//
+
+import Foundation
+import MapKit
+
+struct Location: Codable, Equatable, Identifiable {
+    let id: UUID
+    var name: String
+    var description: String
+    var latitude: Double
+    var longitude: Double
+    
+    var coordinate: CLLocationCoordinate2D { 
+        // ContentView에서 더 간편하게 나타내기 위해 선언
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    #if DEBUG // 앱 스토어에 release할 때 이 코드는 빠짐
+    static let example = Location(id: UUID(), name: "Buckingham Palace", description: "Lit by over 40,000 lightbulbs.", latitude: 51.501, longitude: -0.141)
+    #endif
+    
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        // custom == function을 선언하지 않으면 두 인스턴스를 비교할 때 모든 프로퍼티를 하나씩 대조함
+        // Location 인스턴스는 unique identifier가 있으므로 id만 비교하면 됨
+        return lhs.id == rhs.id
+    }
+}
