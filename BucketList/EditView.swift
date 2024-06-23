3383cf61b347f05f8a0b8019776e712ba684079d
jstar_00 <102504764+jstar000@users.noreply.github.com>
2024-06-23 22:50:30 +0900

    Project 14 - Sorting Wikipedia results까지 완료(MVVM 적용 전)

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -14,6 +14,14 @@ struct EditView: View {
     var location: Location
     var onSave: (Location) -> Void
     
+    enum LoadingState {
+        case loading, loaded, failed
+        // 케이스의 타입을 명시할 필요는 없음
+        // 열거형의 케이스는 자동으로 해당 enum의 타입, 즉 LoadingState라는 타입을 가짐
+    }
+    @State private var loadingState = LoadingState.loading // loading state 저장
+    @State private var pages = [Page]() // fetch가 끝났을 때 Wikipedia pages 저장
+    
     init(location: Location, onSave: @escaping (Location) -> Void) {
         self.location = location
         self.onSave = onSave
@@ -29,6 +37,23 @@ struct EditView: View {
                     TextField("Place name", text: $name)
                     TextField("Description", text: $description)
                 }
+                
+                Section("Nearby...") {
+                    switch loadingState {
+                    case .loaded:
+                        ForEach(pages, id: \.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ")
+                            + Text(page.description)
+                                .italic()
+                        }
+                    case .loading:
+                        Text("Loading...")
+                    case .failed:
+                        Text("Please try again later")
+                    }
+                }
             }
             .navigationTitle("Place details")
             .toolbar {
@@ -43,6 +68,37 @@ struct EditView: View {
                 }
                 
             }
+            .task {
+                await fetchNearbyPlaces()
+                // 이 함수는 뷰가 나타나자마자 실행돼야 함
+            }
+        }
+    }
+    
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            // data(from:)은 네트워크 요청에 관한 메타데이터와 URL로부터의 데이터를 튜풀로 반환함
+            // 메타데이터는 필요 없으므로 (data, _)와 같이 반환받으면 됨
+            
+            // we got some data back!
+            let items = try JSONDecoder().decode(Result.self, from: data)
+            
+            // success - convert the array values to our pages array
+            pages = items.query.pages.values.sorted()
+            // .values는 딕셔너리의 모든 value를 포함하는 컬렉션을 반환함
+            // sorted()는 호출된 배열을 정렬한 새로운 배열을 반환함(원래 배열은 변하지 않음)
+            loadingState = .loaded
+        } catch {
+            // if we're still here it means the request failed somehow
+            loadingState = .failed
         }
     }
 }
--- BucketList/Result.swift
+++ BucketList/Result.swift
@@ -7,18 +7,32 @@
 
 import Foundation
 
-// 위키피디아에서 장소 데이터 다운로드 받기 위해 선언
-
 struct Result: Codable {
     let query: Query
+    // "query"라는 key에 우리 query의 결과를 담음
 }
 
 struct Query: Codable {
     let pages: [Int: Page]
+    // query 안에는 'pages'라는 딕셔너리가 있으며,
+    // page IDs가 key고 Wikipedia pages 자체가 value로 들어감
 }
 
-struct Page: Codable {
+struct Page: Codable, Comparable {
     let pageid: Int
     let title: String
     let terms: [String: [String]]?
+    // 각 페이지는 coordinate, title, terms 등의 수많은 정보를 포함함
+    
+    static func < (lhs: Page, rhs: Page) -> Bool {
+        return lhs.title < rhs.title
+    }
+    
+    var description: String {
+        terms?["description"]?.first ?? "No further information"
+        // 'description' key를 가진다면 terms[description]은 [String] 타입의 value를
+        // 반환할 거고, 그 배열이 값을 가지면 배열의 첫 번째 요소를 반환함
+        // <- 왜 전체 데이터가 아니라 첫 번째 요소만 반환하지..?
+    }
 }
+
